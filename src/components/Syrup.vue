d010929aba278224a8ec4de476a164684cabbe78
TristianPaquette <155991405+TristianPaquette@users.noreply.github.com>
2025-11-05 00:48:24 -0500

    Update Syrup.vue

--- src/components/Syrup.vue
+++ src/components/Syrup.vue
@@ -1,8 +1,25 @@
-<template>
-  <div class="syrup"></div>
+<<template>
+  <div
+    v-if="type !== 'No Syrup'"
+    class="syrup"
+    :style="{ '--texture-color': syrupColor }"
+  ></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue'
+import syrups from '../data/syrups.json'
+
+// Which syrup was chosen
+const props = defineProps<{ type: string }>()
+
+// Looks up color from syrups.json
+const syrupColor = computed(() => {
+  const match = syrups.find(s => s.name === props.type)
+  return match ? match.color : '#c6c6c6'
+})
+</script>
+
 <style lang="scss" scoped>
 .syrup {
   transform: translateY(400%);
